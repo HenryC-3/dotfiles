83673851703e4b2c27cf8e648272eca4359e18a1
HenryC-3 <2324375439@qq.com>
2022-12-15 16:51:00 +0800

    fix(git): lack of email in commit message generated by auto update script

--- git/scripts/autoUpdateLink.mjs
+++ git/scripts/autoUpdateLink.mjs
@@ -5,11 +5,28 @@ import {
     getStr,
     getSubmoduleInfo,
     updateLink,
+    removeEnv,
 } from "./utils/index.mjs";
 
 const submodulePath = await $`pwd`;
 const submodulePathStr = getStr(submodulePath);
 const rootSuperProjectPathStr = $.env.ROOT_SUPER_PROJECT;
+
+// https://git-scm.com/book/en/v2/Git-Internals-Environment-Variables
+const repositoryLocationsENV = [
+    "GIT_ALTERNATE_OBJECT_DIRECTORIES",
+    "GIT_OBJECT_DIRECTORY",
+    "GIT_INDEX_FILE",
+    "GIT_WORK_TREE",
+    "GIT_CEILING_DIRECTORIES",
+    "GIT_DIR"
+]
+
+// NOTE: make sure git commands execute successfully on other repos https://stackoverflow.com/questions/4043609/getting-fatal-not-a-git-repository-when-using-post-update-hook-to-execut/4532716#4532716
+repositoryLocationsENV.forEach((name) => {
+    removeEnv(name)
+})
+
 
 try {
     await autoUpdateLinkRecursively(submodulePathStr, rootSuperProjectPathStr);
@@ -54,6 +71,6 @@ async function autoUpdateLinkRecursively(
 
     // exit when super-project is root super-project
     if (!isRootSuperProject) {
-        autoUpdateLinkRecursively(superProjectPathStr, rootSuperProjectPathStr);
+        await autoUpdateLinkRecursively(superProjectPathStr, rootSuperProjectPathStr);
     }
 }
--- git/scripts/utils/index.mjs
+++ git/scripts/utils/index.mjs
@@ -5,7 +5,7 @@
 export async function isSubmodule(absPath) {
     if (absPath) {
         const result =
-            await $`env -i git -C ${absPath} rev-parse --show-superproject-working-tree`;
+            await $`git -C ${absPath} rev-parse --show-superproject-working-tree`;
         return Boolean(getStr(result));
     }
     return false;
@@ -17,7 +17,7 @@ export async function isSubmodule(absPath) {
  */
 export async function getSubmoduleInfo(submodulePathStr) {
     const superProjectPath =
-        await $`env -i git -C ${submodulePathStr} rev-parse --show-superproject-working-tree`;
+        await $`git -C ${submodulePathStr} rev-parse --show-superproject-working-tree`;
 
     const superProjectPathStr = getStr(superProjectPath);
     const relSubmodulePathStr = submodulePathStr.replace(
@@ -60,12 +60,22 @@ export async function updateLink(
 ) {
     const updateMsg = `auto update: ${relSubmodulePathStr}/`;
     echo("-----------------------------update start-----------------------------")
-    await $`env -i git -C ${superProjectPathStr} add ${submodulePathStr}`;
-    await $`env -i git -C ${superProjectPathStr} commit -m ${updateMsg}`;
+    await $`git -C ${superProjectPathStr} add ${submodulePathStr}`;
+    await $`git -C ${superProjectPathStr} commit -m ${updateMsg}`;
     try {
-        await $`env -i git -C ${superProjectPathStr} push origin master`;
+        await $`git -C ${superProjectPathStr} push origin master`;
     } catch (error) {
         console.log(chalk.yellow(error));
     }
     echo("-----------------------------update end-----------------------------")
+}
+
+/**
+ * 
+ * @param {string} name environment variable name
+ * @returns {string} environment variable value
+ */
+export function removeEnv(name) {
+    delete $.env[name]
+    return $.env[name]
 }
