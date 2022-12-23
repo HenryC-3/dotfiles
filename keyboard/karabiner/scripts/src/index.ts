364abd86d4f4057b9293e4a4cf6978c0ed531a73
HenryC-3 <2324375439@qq.com>
2022-12-23 20:08:29 +0800

    dotfile(update): remove unused profiles

--- keyboard/karabiner/scripts/src/index.ts
+++ keyboard/karabiner/scripts/src/index.ts
@@ -1,20 +1,9 @@
-import { hyperNavigation } from "./profiles/caplock-enhance/hypeNavigation";
-import { getSpaceModifier, getArrow } from "./profiles/palmS/mapArrow";
-import { mapDeleteBackspace } from "./profiles/palmS/mapDeleteBackspace";
-import { mapRedoUndoCopyPaste } from "./profiles/caplock-enhance/mapRedoUndoCopyPaste";
 import {
     getRuleNavigation,
     getRuleSelect,
     getRuleDelete,
 } from "./profiles/thumbs/index";
 
-export const mapHyperNavigation = hyperNavigation;
-export const mapUndoRedoCopyPaste = mapRedoUndoCopyPaste;
-export const mapBackSpaceDelete = mapDeleteBackspace;
-export const mapArrow = {
-    getSpaceModifier,
-    getArrow,
-};
 export const thumbs = {
     getRuleNavigation,
     getRuleSelect,
